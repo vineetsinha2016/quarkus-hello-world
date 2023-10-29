18ebf5dd5da28618cba0f882310df31e4b00241d
vineetsinha2016 <vineet.sinha07@gmail.com>
2023-10-29 19:21:54 -0400

    First Commit quarkus hw

---
+++ src/main/java/org/test/GreetingResource.java
@@ -0,0 +1,22 @@
+package org.test;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+    @GET
+    @Path("/world")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
